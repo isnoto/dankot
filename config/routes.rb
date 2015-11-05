b1ebecf0c13719c375a44e9fde720331f529ccdd
Andrew <honestua@gmail.com>
2015-11-05 13:13:57 +0100

    Add routes for admin scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get '/portfolio' => 'static_pages#portfolio'
-  get '/price'     => 'static_pages#price'
-  get '/about'     => 'static_pages#about'
-  get '/contacts'  => 'static_pages#contacts'
+  scope :admin, module: "admin" do
+    resources :main_page_photos, :portfolio_photos, :categories
+  end
+
+  get 'admin',     to: 'admin#index'
+  get 'portfolio', to: 'static_pages#portfolio'
+  get 'price',     to: 'static_pages#price'
+  get 'about',     to: 'static_pages#about'
+  get 'contacts',  to: 'static_pages#contacts'
 end
